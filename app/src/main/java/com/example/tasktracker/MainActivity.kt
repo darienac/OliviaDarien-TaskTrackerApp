c6ef4a8a77f0a06addd9996b94e4e3be8602a7dd
olivia-ma <oliviama01890@gmail.com>
2024-09-24 01:49:12 -0400

    made the task entries scrollable, and made the clear completed button stationary

--- app/src/main/java/com/example/tasktracker/MainActivity.kt
+++ app/src/main/java/com/example/tasktracker/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.displayCutoutPadding
@@ -13,12 +14,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -29,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.SemanticsProperties.ToggleableState
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
@@ -75,11 +82,11 @@ fun TaskEntry(label: String, isChecked: Boolean, onCheckedChange: (Boolean) -> U
         )
     }
 
-
 }
 
 @Composable
 fun App(modifier: Modifier = Modifier) {
+
     var newTask by remember {mutableStateOf("")}
     // holds name of task and whether it is completed or not
     val tasks = remember {mutableStateListOf<Pair<String, Boolean>>()}
@@ -101,74 +108,98 @@ fun App(modifier: Modifier = Modifier) {
         tasks.addAll(tasksToKeep)
     }
 
-    Column(modifier = modifier.displayCutoutPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        if (error){
-            // error dialog
-            Dialog(onDismissRequest = { error = false }) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .padding(16.dp),
-                    shape = RoundedCornerShape(16.dp),
-                ) {
-                    Text(
-                        text = "Please enter a task.",
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            BottomAppBar(
+                containerColor = Color.Transparent
+            ) {
+                Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+                ){
+                    // Clear button
+                    Button(onClick= {clearCompletedTasks()}
+//            modifier =  Modifier
+//                .padding(8.dp)
+                    ) {
+                        Text("Clear Completed")
+                    }
+                }
+
+            }
+        },
+    ){ innerPadding ->
+        Column(modifier = modifier
+            .displayCutoutPadding()
+            .padding(start = 16.dp, end = 16.dp, bottom = innerPadding.calculateBottomPadding()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+            if (error) {
+                // error dialog
+                Dialog(onDismissRequest = { error = false }) {
+                    Card(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .wrapContentSize(Alignment.Center),
-                        textAlign = TextAlign.Center,
-                    )
+                            .fillMaxWidth()
+                            .padding(innerPadding)
+                            .height(200.dp)
+                            .padding(16.dp),
+                        shape = RoundedCornerShape(16.dp),
+                    ) {
+                        Text(
+                            text = "Please enter a task.",
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .wrapContentSize(Alignment.Center),
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            }
+
+            // User input
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    value = newTask,
+                    onValueChange = { newTask = it },
+                    label = { Text("Task Name") },
+                    modifier = Modifier.weight(1f)
+                        .padding(8.dp)
+                )
+                Button(
+                    onClick = {
+                        if (newTask.isEmpty()) {
+                            // insert a error pop up telling user to insert a valid task
+                            error = true
+                        } else {
+                            tasks.add(Pair(newTask, false))
+                        }
+                        newTask = ""
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                ) {
+                    Text("Add Task")
+                }
+            }
+            // Tasks
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                for (index in 0..(tasks.size - 1)) {
+                    val (task, checked) = tasks[index]
+                    // whenever the checkboxes are interacted with, tasks state gets updated, updating the checkboxes
+                    TaskEntry(task, checked, { tasks[index] = Pair(task, it) })
                 }
             }
         }
-
-        // User input
-        Row(modifier = Modifier
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(
-                value = newTask,
-                onValueChange = {newTask = it},
-                label = {Text("Task Name")},
-                modifier = Modifier.weight(1f)
-                        .padding(8.dp)
-            )
-            Button(onClick={
-                if (newTask.isEmpty()){
-                    // insert a error pop up telling user to insert a valid task
-                    error = true
-                    }
-                else{
-                    tasks.add(Pair(newTask, false))
-                    }
-                newTask = ""
-                },
-                modifier =  Modifier
-                    .padding(8.dp)) {
-                Text("Add Task")
-            }
-        }
-        // Tasks
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ){
-            for (index in 0..(tasks.size-1)){
-                val (task, checked) = tasks[index]
-                // whenever the checkboxes are interacted with, tasks state gets updated, updating the checkboxes
-                TaskEntry(task, checked, {tasks[index] = Pair(task, it)})
-            }
-        }
-        // Clear button
-        Button(onClick= {clearCompletedTasks()}
-//            modifier =  Modifier
-//                .padding(8.dp)
-                ) {
-            Text("Clear Completed")
-        }
-
     }
 }
 
